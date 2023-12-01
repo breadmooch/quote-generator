96398d4aeb8ba02c552a949477ecf2be21a65aab
mo speight <you@example.com>
2023-11-30 19:19:00 -0500

    initializes newQuote function

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let apiQuotes = [];
 
+//Show New Quote
+function newQuote(){
+  //Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+  console.log(quote); 
+}
+
 // Get Quotes from API
 async function getQuotes(){
     const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
    try {
        const response = await fetch(apiUrl);
        apiQuotes = await response.json();
-       console.log(apiQuotes);
+       newQuote();
    } catch(error){
     // Catch Error Here
    }
