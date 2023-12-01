d6590813efd278341b4b32e0cb276e34d45e9fe8
mo speight <you@example.com>
2023-11-30 22:13:53 -0500

    Fixes bug in code

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ const newQuoteBtn = document.getElementById('new-quote');
 function newQuote(){
   //Pick a random quote from apiQuotes array
   const quote = localQuotes[Math.floor(Math.random()*localQuotes.length)];
-  //Check id Author field is blank, replaces it with quote unknown
+  //Check id Author field is blank, replace it with 'unknown'
   if(!quote.author){
     authorText.textContent="unknown";
   } else {
     authorText.textContent = quote.author;
   }
    //Check quote length to determine the styling
-  if(quote.text.length>120){
+  if(quote.text.length > 120){
     quoteText.classList.add('long-quote');
   } else {
     quoteText.classList.remove('long-quote');
@@ -39,13 +39,13 @@ function newQuote(){
 
 //Tweet Quote
 function tweetQuote() {
-    const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(titterUrl,'_blank');
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl,'_blank');
 }
 
 //Event Listeners
-newQuoteBtn.addEventListener('click', newQuote)
-twitterBtn.addEventListener('click',tweetQuote)
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // //On load
 // getQuotes();
