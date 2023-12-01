f511cf9e8999879c2d73ee2adced4160002ce6aa
mo speight <you@example.com>
2023-12-01 00:27:21 -0500

    Cleans code

--- script.js
+++ script.js
@@ -7,21 +7,19 @@ const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-//Show Loading
-function loading(){
+function showloadingSpinner(){
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-//Hide Loading
-function complete(){
+function removeLoadingSpinner(){
     quoteContainer.hidden= false;
     loader.hidden = true;
 }
 
 //Show New Quote
 function newQuote(){
-    loading();
+    showloadingSpinner();
   //Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
   //Check id Author field is blank, replace it with 'unknown'
@@ -38,12 +36,12 @@ function newQuote(){
   }
   // Set Quote, Hide Loader
   quoteText.textContent = quote.text;
-  complete();
+  removeLoadingSpinner();
 }
 
 // Get Quotes from API
 async function getQuotes(){
-    loading();
+    showloadingSpinner();
     const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
    try {
        const response = await fetch(apiUrl);
