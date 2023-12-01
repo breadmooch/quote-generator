6b307554b6688e6f25f1b1b10abc30f1b831522a
mo speight <you@example.com>
2023-11-30 20:28:16 -0500

    Changes answer to author

--- quotes.js
+++ quotes.js
@@ -1,66 +1,66 @@
 const localQuotes =[
     { 
         text:"I shimmer and shine, a weave of arcane art, in scrolls I'm trapped, yet I tear realms apart. What am I?",
-        answer:"Spell",
+        author:"Spell",
     },
     { 
         text:"Born from a staff or a wizard's hand, I'm a force unseen across the land. Cast me with words, bring magic to bear, what am I?",
-        answer:"Incantation",
+        author:"Incantation",
     },
     { 
         text:"My home is in tomes, in libraries grand, wielded by sages who study the land. I'm bound by the learned, with secrets untold, what am I?",
-        answer:"Grimoire",
+        author:"Grimoire",
     },
     {
         text:" I dance in the fire, I whisper in the air, a mystical force beyond compare. I'm summoned by wizards, both wise and old, what am I?",
-        answer:"Elemental",
+        author:"Elemental",
     },
     {
         text:"A cloak of shadows, unseen in the night, I hide in the darkness, out of plain sight. What am I?",
-        answer:"Invisibility",
+        author:"Invisibility",
     },
     {
         text:"A cloak of shadows, unseen in the night, I hide in the darkness, out of plain sight. What am I?",
-        answer:"Weave",
+        author:"Weave",
     },
     {
         text:"I'm a key to the realms beyond, a portal's gift, a magic bond. Step through me, where do you land? What am I?",
-        answer:"Gate",
+        author:"Gate",
     },
     {
         text:" I'm a mirror of the arcane, reflecting the unseen, revealing the hidden. What am I?",
-        answer:"Scrying",
+        author:"Scrying",
     },
     {
         text:"I'm a potion of might, a liquid so rare, drink me with care, for power to bear. What am I?",
-        answer:"Exlixir",
+        author:"Exlixir",
     },
     {
         text:"I'm a creature of magic, with scales that gleam, in dungeons and lairs, I guard with esteem. What am I?",
-        answer:"Dragon",
+        author:"Dragon",
     },
     {
         text:"A celestial body, in the night sky I gleam, a symbol of magic, a wizard's dream. What am I?",
-        answer:"Moon",
+        author:"Moon",
     },
     {
         text:"I'm a circle of power, on the ground I'm laid, within my borders, magic is made. What am I?",
-        answer:"Glyph",
+        author:"Glyph",
     },
     {
         text:" I'm a puzzle of symbols, a cryptic array, decipher me right, and magic will sway. What am I?",
-        answer:"Rune",
+        author:"Rune",
     },
     {
         text:"A feathered quill, in a wizard's hand, I craft spells, across the land. What am I?",
-        answer:"Writing implement",
+        author:"Writing implement",
     },
     {
         text:"I'm a staff of might, with a gem at the head, channel through me, spells to be spread. What am I?",
-        answer:"Arcane Focus",
+        author:"Arcane Focus",
     },
     {
         text:"A magical melody, notes in the air, I weave illusions, beyond compare. What am I?",
-        answer:"Enchantment",
+        author:"Enchantment",
     }
 ]
